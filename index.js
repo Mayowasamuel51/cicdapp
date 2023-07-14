701656bb4741395e2c120e93800156802a735c04
mayowasamuel <87167237+Mayowasamuel51@users.noreply.github.com>
2023-07-14 10:28:38 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+// env connections
+const dotenv = require('dotenv');
+const dotenvb = require('dotenv').config();
+
+// template engines connection
+app.use(express.urlencoded({ extended: true }));
+app.engine('hbs', engine({ extname: '.hbs', defaultLayout: "index" }));
+app.set('view engine', 'hbs');
+
+// working with path..
+const path = require('path')
+
+// Make public folder globally 
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+//routes
+
+
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log('server is running ')
+})
